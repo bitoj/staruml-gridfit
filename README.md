f8792912c5709fd1f4895169509022f6539854be
Ronald Smits <72045857+bitoj@users.noreply.github.com>
2020-11-02 09:28:30 +0100

    expand width and height to grid without movement

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ GridFit extension
 ====================
 
 This is a StarUML 3+ extension for aligning non-parasitic nodes ("boxes") in diagrams to the grid.
+
+The current version is only a proof of concept, limited to changing the size without moving nodes to the grid. 
+Also lacking is a check whether nodes are actually resizable.
--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-function expandToGrid () {
-  let gridSize = app.preferences.get('diagramEditor.gridSize');
-  window.alert('grid size: ' + gridSize);
+function adjustedSize(roundingFunc, gridSize, minCoord, minSize, currentSize, maxCoord) {
+  let lineWidth = minCoord + currentSize - maxCoord;
+  let newSize = gridSize * roundingFunc((currentSize - lineWidth) / gridSize) + lineWidth;
+  if (newSize < minSize) {
+    newSize = gridSize * roundingFunc((minSize - lineWidth) / gridSize) + lineWidth;
+  }
+  newSize = Math.max(newSize, gridSize + lineWidth);
+  return newSize;
 }
 
-function init () {
+function adjustNode(roundingFunc, gridSize, node) {
+  let newSize = adjustedSize(roundingFunc, gridSize, node.left, node.minWidth, node.width, node.getRight());
+  node.width = newSize;
+  newSize = adjustedSize(roundingFunc, gridSize, node.top, node.minHeight, node.height, node.getBottom());
+  node.height = newSize;
+}
+
+function adjustSelectedNodes(roundingFunc) {
+  let gridSize = app.preferences.get('diagramEditor.gridSize');
+  let nodeViewType = app.type.NodeView;
+  let parasiticViewType = app.type.ParasiticView;
+  let selectedViews = app.selections.getSelectedViews()
+  selectedViews.forEach(element => {
+    if (element instanceof nodeViewType && !(element instanceof parasiticViewType)) {
+      adjustNode(roundingFunc, gridSize, element);
+    }
+  });
+  app.diagrams.repaint();
+}
+
+function expandToGrid() {
+  adjustSelectedNodes(Math.ceil);
+}
+
+function init() {
   app.commands.register('gridfit:expand', expandToGrid);
 }
 
